2d2cf1338e78507df0c94cd317776bbc3458090d
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-07 08:51:42 -0700

    the betty is mad

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - This function main prints the string provided to it
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("Programming is like building a multilingual puzzle \n");
+	return (0);
 }
