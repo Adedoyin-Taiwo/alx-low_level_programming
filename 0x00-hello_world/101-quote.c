8f6cb09959b5600f24f0e260064726d2eb332375
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-07 16:09:09 -0700

    On God

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
+
 /**
  * main - the function prints a string.
  * Return: 1
  */
 int main(void)
 {
-	constant char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t bytes_written = write(2, message, strlen(message));
-
-	if (bytes_written == -1)
-	{
-	return (1);
-	}
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	return (0);
 }
