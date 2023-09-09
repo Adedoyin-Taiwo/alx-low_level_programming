45a8fc339210afa6e1465624eeeb555dc5a50cb0
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-09 04:08:31 -0700

    3-print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - prints both lowercase and uppercase
+* Return: 0
+*/
+int main(void)
+{
+	char letter, letterA;
+
+	letter = 'a';
+	letterA = 'A';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	while (letterA <= 'Z')
+	{
+		putchar(letterA);
+		letterA++;
+	}
+	return (0);
+}
