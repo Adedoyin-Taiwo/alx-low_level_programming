8e1edbf7a72361258ad6efaa8afb8157a808e6ba
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-09 15:17:18 -0700

    charisma

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,51 @@
 #include <stdio.h>
+
+/**
+* main - The function prints two two digits
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+	int ddig [4] = {0};
+	int c;
+	int dcomaspace[] = {44, 32};
+	int z;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j < 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (l = j; l <= 57; l++)
+				{
+					ddig[0] = i;
+					ddig[1] = j;
+					ddig[2] = k;
+					ddig[3] = l;
+
+					for (c = 0; c < 2; c++)
+					{
+						putchar(ddig[c]);
+					}
+					putchar(' ');
+					for (c = 2; c < 4; c++)
+					{
+						putchar(ddig[c]);
+					}
+					for (z = 0; z < 2; z++)
+					{
+						putchar(dcomaspace[z]);
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
+
+
