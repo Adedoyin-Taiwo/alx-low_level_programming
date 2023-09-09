5a4cc467ef9d4b3c0851ede1515f72bf823d43e6
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-09 09:17:17 -0700

    commit9p

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - prints digits in ascending order
+* Return: 0
+*/
+int main(void)
+{
+	int d = 48;
+	int dcomaspace [] = {44, 32};
+
+	while (d <= 56)
+	{
+		putchar(d);
+		for (int i = 0; i < sizeof(dcomaspace) / sizeof(dcomaspace[0]); i++)
+		{
+			putchar(dcomaspace[i]);
+		}
+		d++;
+	}
+	putchar(57);
+	putchar('\n');
+	return (0);
+}
