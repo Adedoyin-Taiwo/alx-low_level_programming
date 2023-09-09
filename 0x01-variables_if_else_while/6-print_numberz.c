58ccd3b9bf20a55b62db2fc4b600758ec312bf16
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-09 07:15:46 -0700

    6p

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - the function prints digits
+* Return: 0
+*/
+int main(void)
+{
+	int d = 48;
+
+	while (d <= 57)
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
