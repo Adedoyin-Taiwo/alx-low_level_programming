dd280d8faa01bbc468d6cbc40d00a46ace95ff00
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-12 15:19:47 -0700

    54

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_last_digit - prints last digit
+* Return: 0
+* @n: inputted integer
+*/
+
+int print_last_digit(int n)
+{
+	int lastd;
+
+	lastd = n % 10;
+
+	return (lastd);
+}
