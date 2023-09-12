979dc6c36a70cbf3081ce530ad2561796926fce3
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-12 07:51:50 -0700

    we move

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
-* print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line.
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase
 * return: Null
 */
 
 void print_alphabet_x10(void)
 {
 	int ch;
-	
 	int i = 0;
+
 	while (i < 10)
 	{
-		for (ch = 'a'; ch < 'z'; ch++)
+		ch = 'a';
+		while (ch < 'z')
 		{
 			_putchar(ch);
+			ch++;
 		}
+		i++;
 		_putchar('\n');
 	}
 }
