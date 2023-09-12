ca6c58bf3a50ad880bab9d1b086a3992d8a19987
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-12 07:25:46 -0700

    commit add 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line.
+* return: Null
+*/
+
+void print_alphabet_x10(void)
+{
+	int ch;
+	
+	int i = 0;
+	while (i < 10)
+	{
+		for (ch = 'a'; ch < 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
