a8927587272b5008ba099fca058331b4289f4c60
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-13 16:39:07 -0700

    attracted

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,35 +1,35 @@
+#include "main.h"
 #include <stdio.h>
 
-int main (int n)
-{
-	int d;
+/**
+* print_to_98 - prints natural number
+* Return: 0
+* @n: natural number
+*/
 
-	n = -100 ;
+void print_to_98(int n)
+{
 	if ((n - 98) > 0)
 	{
-		do
-		{
-			printf("%d",n );
+		do {
+			printf("%d", n);
 			if (n != 98)
 			{
 				printf(", ");
 			}
 			n -= 1;
-		}
-		while (n >= 98);
+		} while (n >= 98);
 	}
 	else if ((n - 98) <= 0)
 	{
-		do		
-		{
-			printf("%d",n );
+		do {
+			printf("%d", n);
 			if (n != 98)
 			{
 				printf(", ");
 			}
 			n += 1;
-		}
-		while (n <= 98);
+		} while (n <= 98);
 	}
 	printf("\n");
 }
