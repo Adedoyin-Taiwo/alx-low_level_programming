93c4f35a03dcea4798c66ec28c743c8f49789bbd
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-14 14:05:07 -0700

    1-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* more_numbers - 10 times  0 -14
+* Return: 0
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (i <= 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			if (j < 10)
+			{
+				_putchar(j + '0');
+			}
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
