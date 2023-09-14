70584e0cb40d27dd04ba9dd55a570102ed875408
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-13 17:17:59 -0700

    commitP

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* print_times_table - print times table
+* @n: integer
+* Return: 0
+*/
+
+void print_times_table(int n)
+{
+	int ddig;
+	int i, j;
+	
+	i = 0;
+	while (i <= n)
+	{
+		j = 0;
+		while (j <= n)
+		{
+			ddig = i * j;
+			if (ddig >= 10)
+			{
+				printf(", ");
+				printf("%d", ddig);
+			}
+			else
+			{
+				if (j != 0)
+				{
+					printf(",  ");	
+				}
+				printf("%d", ddig )
+			}
+			j++;
+			printf("\n");
+		}
+		i++;
+	}
+}
