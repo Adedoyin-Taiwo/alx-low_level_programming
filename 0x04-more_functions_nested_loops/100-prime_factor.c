9c9e6c5b836c78c4d20c308b478f8293d88116e5
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-14 17:27:38 -0700

    Comoto

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - prints highest prime factor of 612852475143
+* Return: 0
+*/
+
+int main(void)
+{
+	long int  large_num = 612852475143;
+	long int larg_prime;
+
+	for (larg_prime = 1; larg_prime <= large_num; large_num++);
+	{
+		if (large_num % larg_prime == 0)
+		{
+			large_num = large_num / larg_prime;
+		}
+	}
+	printf("%ld\n", larg_prime);
+	return (0);
+}
