e45f1fddfcde4db2e1be9f636300255284bebe7b
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-21 03:42:15 -0700

    string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_rev - print reverse
+* return: 0
+* @s: the string
+*/
+
+void print_rev(char *s)
+{
+	int mov = strlen(s) - 1;
+
+	while (mov >= 0)
+	{
+		_putchar(s[mov]);
+		--mov;
+	}
+}
