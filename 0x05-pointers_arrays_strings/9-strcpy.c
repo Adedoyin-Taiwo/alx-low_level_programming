25161b12a922ebcdf9849458ad0c6c22204fdf10
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-21 08:22:57 -0700

    retreat

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strcpy - copies the string pointed
+* @dest: buffer
+* @src: ointed to
+* Return: the pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i < strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
