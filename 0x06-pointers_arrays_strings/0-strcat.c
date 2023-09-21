ba71530fa4c4a231b0baa4ea487ce5a33c5c18a6
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-21 09:54:30 -0700

    commit it

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "main.h"
+
+/**
+* _strcat - concatenate strings
+* @dest: destination string
+* @src: source string
+* Return: pointer to the resulting string dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j = 0;
+	int len1 = 0;
+
+	while (dest[len1] != '\0')
+	{
+		len1++;
+	}
+
+	for (i = len1; dest[j] != '\0'; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ This readme fileis for this directory
