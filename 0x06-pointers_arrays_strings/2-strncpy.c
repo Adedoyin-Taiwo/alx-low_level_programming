0d5ca24fe24347967bae469be134a914e0f6c779
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-21 23:01:45 -0700

    Gogogo

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,15 +13,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < strlen(dest))
+	while (i < n && src[i] != '\0')
 	{
 
 		dest[i] = src[i];
-		if (i < n)
-		{
 			i++;
-		}
 	}
+	while (i < n)
+	{
 	dest[i] = '\0';
+	i++;
+	}
 	return (dest);
 }
