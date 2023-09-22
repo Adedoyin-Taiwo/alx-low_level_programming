ae964bf27cad84963eb9389d6ca1d036c2098486
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-22 06:30:03 -0700

    faster

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* leet - the leet converter
+* @str: the string
+* Return: char
+*/
+
+char *leet(char *str)
+{
+	char arr_char[] = "oOlLeEaAtT"
+	char arr_num[] = "0011334477";
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; arr_char[j] != '\0'; j++)
+		{
+			if (str[i]  == arr_char[j])
+			{
+				str[i] = arr_num[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
