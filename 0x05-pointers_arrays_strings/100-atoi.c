43d0059401f666af609a6b7423537df12a73db56
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-24 12:45:17 -0700

    DDAR

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,10 +11,10 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int result = 0;
+	signed long int result = 0;
 	int sign = 1;
 
-	while ((*(s + i) < '0' || *(s + i) > '9') && *(s + i ) != '\0')
+	while ((*(s + i) < '0' || *(s + i) > '9') && *(s + i) != '\0')
 	{
 		if (*(s + i) == '-')
 		{
@@ -22,7 +22,7 @@ int _atoi(char *s)
 		}
 		i++;
 	}
-	while (*(s + i) >= '0' && *(s + i) <= '9' && *(s + i ) != '\0')
+	while (*(s + i) >= '0' && *(s + i) <= '9' && *(s + i) != '\0')
 	{
 		result = result * 10 + (s[i] - '0');
 		i++;
