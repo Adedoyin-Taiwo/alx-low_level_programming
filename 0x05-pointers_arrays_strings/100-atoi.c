9b58f51b78e353bf8d14443c39248f384edb5237
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-24 12:18:42 -0700

    ATM

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,20 +10,23 @@
 
 int _atoi(char *s)
 {
-	int i;
+	int i = 0;
 	int result = 0;
+	int sign = 1;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	while ((*(s + i) < '0' || *(s + i) > '9') && *(s + i ) != '\0')
 	{
-		
-		if ((*(s + (i + 1)) >= '0' && *(s + (i+1)) <= '9') && *(s + (i + 1)) != '\0')
+		if (*(s + i) == '-')
 		{
-			result = result * 10 + (s[i] - '0');
+			sign *= -1;
 		}
-		else if (*(s + i) >= '0' && *(s + i) <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-		}
+		i++;
 	}
-	return (0);
+	while (*(s + i) >= '0' && *(s + i) <= '9' && *(s + i ) != '\0')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	result *= sign;
+	return (result);
 }
