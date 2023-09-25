5a74dd8d432428813b1a935de5abde0827c2e79d
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-25 13:33:14 -0700

    main

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,13 +11,13 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+	int i, j;
 
-	for (i = 0; *(accept + i) != '\0'; i++)
+	for (j = 0; *(s + j) != '\0'; j++)
 	{
-		for (j = 0; *(s + j) != '\0'; j++)
+		for (i = 0; *(accept + i) != '\0'; i++)
 		{
-			if (*(accept + i) == *(s + j))
+			if (*(s + j) == *(accept + i))
 			{
 				return (s + j);
 			}
