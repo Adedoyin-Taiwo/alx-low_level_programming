d788963a6e94e2ae24d7584693afa34fc2abeab2
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-25 22:06:38 -0700

    kkk

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,14 +16,8 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		if (i % (size + 1) == 0)
-		{
-			lead_sum += *(a + i);
-		}
-		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-		{
-			opp_sum += *(a + i);
-		}
+		lead_sum += *(a + (size * i + i));
+		opp_sum += *(a + (size * i + size - 1 - i));;
 	}
 	printf("%d, ", lead_sum);
 	printf("%d\n ", opp_sum);
