55c422537bba6551f0bb6fe316bb1adfa9204c8a
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-25 20:49:58 -0700

    diagsum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_diagsums - prints diagonal sums
+* @a: array
+* @size: size of the array
+* Returns: 0
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int lead_sum = 0;
+	int opp_sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		lead_sum += a[i * size + i];
+		opp_sum += a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d", lead_sum, opp_sum);
+}
