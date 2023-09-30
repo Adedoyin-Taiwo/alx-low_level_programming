4c5ac835f71e5f56e58ec630e17a8140d788a364
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-09-30 06:45:43 -0700

    testit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,1 +1,36 @@
 #include "main.h"
+
+/**
+* string_test - test the strings
+* @s1: first string
+* @s2: second string
+* Return: 0
+*/
+
+int string_test(char *s1, char *s2)
+{
+	if (*s1 == '\0' &&  *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (string_test(s1 + 1, s2 + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+* wildcmp - compares two strings
+* @s1: first string
+* @s2: second string
+* Return: 0
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	return (string_test(si, s2));
+}
