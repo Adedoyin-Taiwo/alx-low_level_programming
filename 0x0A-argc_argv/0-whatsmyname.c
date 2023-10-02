1403895a29748ab2669a1a83c7b074e454aeb6e2
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-02 13:37:22 -0700

    Commititto

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Prints the name of the program, then a new line.
+* @argc: Arguments.
+* @argv: Pointers to the arguments.
+* Return: 0
+*/
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
