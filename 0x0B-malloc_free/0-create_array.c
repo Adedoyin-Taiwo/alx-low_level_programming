ea0cf2ab9f1a48b34e787986799a9c22447e4900
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-04 02:35:22 -0700

    KKH

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,26 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-char *hold_mal(unsigned int size, char c, char *d)
-{
-	unsigned int i;
-
-	d = malloc(sizeof(char)*size);
-	if (size == 0)
-	{
-		return (NULL);
-	}
-	while (i < size)
-	{
-		c[i] == c;
-		return (mal(c + 1));
-	}
-	if (c == Null)
-	{
-		return (NULL);
-	}
-}
 /**
 * create_array - creates an array of characters
 * @size: size of the array
@@ -30,5 +10,22 @@ char *hold_mal(unsigned int size, char c, char *d)
 
 char *create_array(unsigned int size, char c)
 {
-	return (mal(char * c);
+	unsigned int i;
+	char *d;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	d = malloc(sizeof(char) * size);
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		d[i] = c;
+	}
+	return (d);
 }
+
