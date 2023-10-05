d8e560062dcf489454f72c06a3b06822cb25abcf
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-05 08:12:59 -0700

    The Giddy

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,63 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+* argstostr -  
+
 char *argstostr(int ac, char **av)
 {
-    if (ac == 0 || av == NULL)
-    {
-        return NULL;
-    }
-
-    int total_length = 0;
-    
-    // Calculate the total length of the concatenated string
-    for (int i = 0; i < ac; i++)
-    {
-        if (av[i] != NULL)
-        {
-            total_length += strlen(av[i]) + 1; // +1 for the newline character
-        }
-    }
-
-    char *result = (char *)malloc(total_length + 1); // +1 for the null terminator
-    
-    if (result == NULL)
-    {
-        return NULL;
-    }
-
-    int position = 0;
-
-    // Copy each argument followed by a newline character
-    for (int i = 0; i < ac; i++)
-    {
-        if (av[i] != NULL)
-        {
-            strcpy(result + position, av[i]);
-            position += strlen(av[i]);
-            result[position++] = '\n';
-        }
-    }
-
-    result[total_length] = '\0'; // Null-terminate the final string
-
-    return result;
-}
-
-int main(int argc, char **argv)
-{
-    char *concatenated = argstostr(argc, argv);
-
-    if (concatenated != NULL)
-    {
-        printf("%s", concatenated);
-        free(concatenated); // Don't forget to free the memory when done
-    }
-    else
-    {
-        printf("Memory allocation failed.\n");
-    }
-
-    return 0;
-}
  
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,128 +3,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Helper function to count the number of words in a string
-int count_words(char *str)
-{
-    int count = 0;
-    int is_word = 0;
-
-    while (*str)
-    {
-        if (*str == ' ' || *str == '\t' || *str == '\n')
-        {
-            is_word = 0;
-        }
-        else if (!is_word)
-        {
-            count++;
-            is_word = 1;
-        }
-
-        str++;
-    }
-
-    return count;
-}
-
+/**
+* strtow - splits a string into words
+* @str: the string
+* Return: pointer to the array
+*/
 char **strtow(char *str)
 {
-    if (str == NULL || *str == '\0')
-    {
-        return NULL;
-    }
+	int i, j, k;
+	char **strtow;
 
-    int num_words = count_words(str);
-
-    char **word_array = (char **)malloc((num_words + 1) * sizeof(char *));
-
-    if (word_array == NULL)
-    {
-        return NULL; // Memory allocation failed
-    }
-
-    int word_index = 0;
-    int word_length = 0;
-    int is_word = 0;
-
-    for (int i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-        {
-            if (is_word)
-            {
-                word_array[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-                if (word_array[word_index] == NULL)
-                {
-                    // Memory allocation failed, free previously allocated memory
-                    for (int j = 0; j < word_index; j++)
-                    {
-                        free(word_array[j]);
-                    }
-                    free(word_array);
-                    return NULL;
-                }
-
-                strncpy(word_array[word_index], &str[i - word_length], word_length);
-                word_array[word_index][word_length] = '\0';
-                word_index++;
-                word_length = 0;
-                is_word = 0;
-            }
-        }
-        else
-        {
-            word_length++;
-            is_word = 1;
-        }
-    }
-
-    // Handle the last word
-    if (is_word)
-    {
-        word_array[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-        if (word_array[word_index] == NULL)
-        {
-            // Memory allocation failed, free previously allocated memory
-            for (int j = 0; j < word_index; j++)
-            {
-                free(word_array[j]);
-            }
-            free(word_array);
-            return NULL;
-        }
-
-        strncpy(word_array[word_index], &str[strlen(str) - word_length], word_length);
-        word_array[word_index][word_length] = '\0';
-        word_index++;
-    }
-
-    word_array[word_index] = NULL; // The last element of the array should be NULL
-
-    return word_array;
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	strtow = (char **)malloc(sizeof(char *) * (strlen(str) + 1));
+	if (strtow == NULL)
+		return (NULL);
+	i = 0;
+	while (*(str + i) != '\0' && i < strlen(str))
+	{
+		strtow[i] = (char *)malloc(strlen(str) + 1);
+		if (strtow[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(strtow[j]);
+			}
+			free(strtow);
+			return (NULL);
+		}
+		if (*(str + i) == ' ' && (i == 0 || *(str + (i - 1)) != ' '))
+		{
+			k = 0;
+			while (*(str + i) != ' ' && *(str + i) != '\0')
+			{
+				strtow[i][k] = *(str + i);
+				i++;
+				k++;
+			}
+			strtow[i][k] = '\0';
+		}
+		else
+		{
+			i++;
+		}
+	}
+	strtow[i] = NULL;
+	return (strtow);
 }
-
-int main()
-{
-    char *input = "This is a test string";
-    char **words = strtow(input);
-
-    if (words != NULL)
-    {
-        int i = 0;
-        while (words[i] != NULL)
-        {
-            printf("%s\n", words[i]);
-            free(words[i]); // Free each word
-            i++;
-        }
-        free(words); // Free the array of words
-    }
-    else
-    {
-        printf("Memory allocation failed or input is empty.\n");
-    }
-
-    return 0;
-}
-
