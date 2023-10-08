65ae9117ff55e042c9d84a4f49a5ba7a9f466efb
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-08 14:48:59 -0700

    Mult

--- 0x0C-more_malloc_free/1011111-mul.c
+++ 0x0C-more_malloc_free/1011111-mul.c
@@ -24,21 +24,24 @@ int main(int argc, char **argv)
 	}
 	num1 = strtoul(argv[1], NULL, 10);
 	num2 = strtoul(argv[2], NULL, 10);
+	printf("num1 num2 %lu %lu \n", num1, num2);
 	mul = num1 * num2;
+	printf("multiple is %lu", mul);
 	while (num1 > 0 || num2 > 0)
 	{
 		dec1char = '0' + (num1 % 10);
 		dec2char = '0' + (num2 % 10);
-		if (isdigit(dec1char) || isdigit(dec2char))
+		if (isdigit(dec1char) && isdigit(dec2char))
 		{
-			num1 = num1 / 10;
-			num2 = num2 / 10;
+			continue;
 		}
 		else
 		{
 			printf("Error\n");
 			exit(98);
 		}
+		num1 = num1 / 10;
+		num2 = num2 / 10;
 	}
 	printf("%lu\n", mul);
 	return (0);
