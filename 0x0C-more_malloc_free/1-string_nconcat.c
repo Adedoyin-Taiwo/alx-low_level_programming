58c3d39e4b472d408c871424f31cd33e40f40580
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-08 02:51:50 -0700

    Free Me

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,17 +26,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	if (n >= strlen(s2))
 		n = strlen(s2);
-	while (*(s1 + i) != '\0')
+	for (i = 0; *(s1 + i) != '\0'; i++)
 	{
 		*(m + i) = *(s1 + i);
-		i++;
 	}
-	while (j < n && *(s2 + j) != '\0')
+	for (j = 0; j < n; j++)
 	{
-		*(m + i) = *(s2 + j);
-		i++;
-		j++;
+		*(m + i + j) = *(s2 + j);
 	}
-	*(m + i) = '\0';
+	*(m + i + j) = '\0';
 	return (m);
 }
