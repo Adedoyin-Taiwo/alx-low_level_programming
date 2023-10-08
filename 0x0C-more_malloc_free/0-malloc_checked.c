b9df1f0f17a1efe3d9ba20eb2924a5cfbe180448
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-07 23:44:23 -0700

    exit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* malloc_checked - function allocates memory using malloc
+* @b: 
+* Return - 0
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int * c;
+
+	c = malloc(sizeof(b));
+	if (c == NULL)
+	{
+		exit(98);
+	}
+}
