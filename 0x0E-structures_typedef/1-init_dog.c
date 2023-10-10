fcbe68300bba58ffaf5af9f00782dd01b98af6a2
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-10 16:06:18 -0700

    E ni sun

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,8 +15,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	char *n, *o;
 
-	n = malloc(sizeof(char) * (strlen(name)) + 1);
-	o = malloc(sizeof(char) * (strlen(owner)) + 1);
+	n = malloc(sizeof(char) * (strlen(name) + 1));
+	o = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (n == NULL)
 		return;
 	if (o == NULL)
