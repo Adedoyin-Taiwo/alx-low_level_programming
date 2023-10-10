c67458e3ae3003d4c09243d5a9099be9d3e429e1
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-10 14:23:17 -0700

    Compiler

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - definesa new type struct dog
+* @name: the name
+* @age: the age
+* @owner: the owner
+*/
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif
