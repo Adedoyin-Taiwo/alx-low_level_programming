11d7f632830f3b1b233e65af77811d2586ecc53e
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-11 12:25:26 -0700

    KHK

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,27 +12,26 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog;
-	char *abname, *abowner; 
 
 	newDog = malloc(sizeof(dog_t));
 	if (newDog == NULL)
 		return (NULL);
-	abname = malloc(sizeof(char) * (strlen(name) + 1));
-	if (abname == NULL)
+	newDog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (newDog->name == NULL)
 	{
+		free(newDog);
 		return (NULL);
-		free (newDog);
 	}
-	abowner = malloc(sizeof(char) * (strlen(owner) + 1));
-	if (abowner == NULL)
+	newDog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (newDog->owner == NULL)
 	{
+		free(newDog->name);
+		free(newDog);
 		return (NULL);
-		free (abname);
-		free (newDog);
 	}
 
-	newDog->name = strcpy(abname, name);
+	newDog->name = strcpy(newDog->name, name);
 	newDog->age = age;
-	newDog->owner = strcpy(abowner, owner);
+	newDog->owner = strcpy(newDog->owner, owner);
 	return (newDog);
 }
