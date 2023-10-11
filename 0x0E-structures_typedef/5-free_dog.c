92108aa098a61283e9b2ed10d56582f68243652a
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-11 12:40:20 -0700

    REE

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+* free_dog - frees dog
+* @d: the structure to be freed
+* Return: nothing
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
