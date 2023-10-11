d5f903dd9fa4a89601485b972a37e545d637fc4b
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-11 10:12:49 -0700

    poppy

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,22 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
 		printf("Age: %f\n", (d->age) ? d->age : 0);
-		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+		if (d->owner != NULL)
+		{
+			printf("Name: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
 	}
 }
