dfe857ef1ef5258a65a4ae8a0f201d586bbc93b8
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-20 12:46:57 -0700

    We go again

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+* print_list - prints the list
+* @h: pointer
+* Return: 0
+*/
+
+size_t print_list(const list_t *h)
+{
+	int counter = 0;
+	const list_t *ptr = h;
+
+	if (h == NULL)
+		return (0);
+	while(ptr != NULL)
+	{
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			counter++;
+			ptr = ptr->next;
+		}
+		else
+		{
+		printf("[%u] %s\n", ptr->len, ptr->str);
+		ptr = ptr->next;
+		counter++;
+		}
+	}
+	return (counter);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory consists of programs on Singly Linked List
