1cf884a9cb84afa54aea915ccad3ee35777ca0de
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-20 18:02:45 -0700

    KKK

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,8 +7,8 @@
 
 void print_intro(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 __attribute__((constructor))
 /**
