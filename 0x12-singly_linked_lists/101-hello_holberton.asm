b1c1f692875c61f0f3acd87cbdb9e70fabc00222
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-21 02:57:47 -0700

    ASM

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,9 @@
-section .data
-    hello db "Hello, Holberton",10,0  ; The string to print, followed by a new line (10) and null terminator (0)
-
-section .text
-    global _start
-
-_start:
-    ; Prepare arguments for printf
-    mov rdi, hello  ; rdi is the first argument (format string)
-    mov rax, 0      ; rax is the syscall number for printf
-    call printf     ; Call printf
-    mov rax, 60     ; rax is the syscall number for exit
-    xor rdi, rdi    ; rdi is the exit status (0)
-    syscall
-
-section .bss
-    ; Define the _start symbol as a global symbol
-    global _start
-
+          global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
