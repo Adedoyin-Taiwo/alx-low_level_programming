55197a3156c38f12fb0989ea44b8c903918b969f
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-24 14:31:12 -0700

    DMAN

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+* delete_nodeint_at_index - as the name implies
+* @head: the head of the linked list
+* @index: where the node should be deleted
+* Return: 1 (success) -1(failed)
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *temp, *ptr;
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
+	ptr = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	while (count < index - 1 && ptr != NULL)
+	{
+		ptr = ptr->next;
+		count++;
+	}
+	if (ptr == NULL)
+		return (-1);
+	temp = ptr->next;
+	ptr->next = temp->next;
+	return (1);
+}
