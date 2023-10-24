55ca47c3b14dd5549839477b5af7366ec643f775
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-24 14:47:38 -0700

    Diab

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,15 +11,13 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int count = 0;
 	listint_t *temp, *ptr;
 
-	temp = malloc(sizeof(listint_t));
-	if (temp == NULL)
-		return (-1);
 	if (head == NULL || *head == NULL)
 		return (-1);
 	ptr = *head;
 	if (index == 0)
 	{
 		*head = (*head)->next;
+		free(ptr);
 		return (1);
 	}
 	while (count < index - 1 && ptr != NULL)
@@ -27,9 +25,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		ptr = ptr->next;
 		count++;
 	}
-	if (ptr == NULL)
+	if (ptr == NULL || ptr->next == NULL)
 		return (-1);
-	temp = ptr->next;
+	*temp = ptr->next;
 	ptr->next = temp->next;
+	free(temp);
 	return (1);
 }
