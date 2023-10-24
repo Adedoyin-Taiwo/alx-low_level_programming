1a3d58fe8ad30a347f78910fd16a02474176def9
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-24 09:47:14 -0700

    Git node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,8 +12,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int count = 0;
 	listint_t *ptr;
 
-	if (index < 0)
-		return (NULL);
 	ptr = head;
 	while (count < index)
 	{
