b409e3780228f95d8def6a03f3ab02573da711e3
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-24 14:50:37 -0700

    Diab

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	if (ptr == NULL || ptr->next == NULL)
 		return (-1);
-	*temp = ptr->next;
+	temp = ptr->next;
 	ptr->next = temp->next;
 	free(temp);
 	return (1);
