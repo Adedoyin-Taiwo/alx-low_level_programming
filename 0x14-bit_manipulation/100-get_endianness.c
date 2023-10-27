e0f6f19ecf9be38939de6548bdeb99c3c440634c
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-10-27 14:22:59 -0700

    GGG

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* get_endianness - check for the  endianness
+* Return: 0 if big endian, or 1 if little endian
+*/
+
+int get_endianness(void)
+{
+	int dig;
+
+	dig = 1;
+	if (*(char *)&dig == 1)
+		return (1);
+	else
+		return (0);
+}
